305e1b2596ca210120689bfddb84553376a7c44d
Subhankar Acharya <35475791+Subhankar89@users.noreply.github.com>
2021-09-19 12:55:59 +0530

    Inital failing test

---
+++ SampleApp/SampleAppTests/APIManagerTest.swift
@@ -0,0 +1,16 @@
+//
+//  APIManagerTest.swift
+//  SampleAppTests
+//
+//  Created by Subhankar  Acharya on 19/09/21.
+//
+
+import XCTest
+
+class APIManagerTest: XCTestCase {
+
+    func test_getUser_WithExpectedURLHostAndPath() {
+        let apiManager = APIManager()
+    }
+
+}
