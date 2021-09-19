48b5355245ccb33af56430f6acfc2b6eba4716e8
Subhankar Acharya <35475791+Subhankar89@users.noreply.github.com>
2021-09-19 13:52:04 +0530

    Added APIManager class

---
+++ SampleApp/SampleApp/Service/APIManager.swift
@@ -0,0 +1,12 @@
+//
+//  APIManager.swift
+//  SampleApp
+//
+//  Created by Subhankar  Acharya on 19/09/21.
+//
+
+import Foundation
+
+class APIManager {
+    
+}
--- SampleApp/SampleAppTests/APIManagerTest.swift
+++ SampleApp/SampleAppTests/APIManagerTest.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import SampleApp
 
 class APIManagerTest: XCTestCase {
 
